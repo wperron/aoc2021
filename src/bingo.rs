0649cc4283919be2249dd5586493e75ea28199be
William Perron <hey@wperron.io>
2021-12-09 21:26:51 -0500

    day 4 WIP

--- src/bingo.rs
+++ src/bingo.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 
+#[derive(Copy, Clone)]
 enum CellState {
     Checked,
     Unchecked,
@@ -11,7 +12,7 @@ impl Default for CellState {
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Copy, Clone)]
 struct Cell {
     val: i32,
     state: CellState,
@@ -35,7 +36,19 @@ impl FromStr for Cell {
     }
 }
 
-type Row<const N: usize> = [Cell; N];
+#[derive(Clone, Copy)]
+struct Row<const N: usize> {
+    inner: [Cell; N],
+}
+
+impl<const N: usize> Default for Row<N> {
+    fn default() -> Self {
+        Self {
+            inner: [Default::default(); N],
+        }
+    }
+}
+
 type Col<const N: usize> = Row<N>;
 
 struct Board<const N: usize> {
@@ -52,7 +65,38 @@ impl<const N: usize> Default for Board<N> {
     }
 }
 
+#[derive(Default)]
 struct Game<const N: usize> {
     boards: Vec<Board<N>>,
     draw: Vec<i32>,
+}
+
+impl<const N: usize> FromStr for Game<N> {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines();
+        let draw: Vec<i32> = lines
+            .next()
+            .ok_or_else(|| std::io::Error::from(std::io::ErrorKind::InvalidData))?
+            .split(',')
+            .map(|s| i32::from_str_radix(s, 10).unwrap())
+            .collect();
+
+        let mut boards: Vec<Board> = vec![];
+        let mut cols: [Col; N] = [Default::default(); N];
+        let mut rows: [Row; N] = [Default::default(); N];
+        for line in lines {
+            // The input contains a few empty lines, this check just skips those.
+            if line.trim().len() == 0 {
+                continue;
+            }
+
+            for num in line.split(" ") {
+                let num = i32::from_str_radix(num.trim(), 10)?;
+            }
+        }
+
+        Ok(Self { draw, boards })
+    }
 }
