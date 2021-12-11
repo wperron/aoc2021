eba0d16f90c55bb2157ee1c528ed67412909199b
William Perron <hey@wperron.io>
2021-12-11 12:16:20 -0500

    fix display for cell state

--- src/bingo.rs
+++ src/bingo.rs
@@ -15,8 +15,8 @@ impl Default for CellState {
 impl Display for CellState {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            CellState::Checked => write!(f, "{}", "⬜"),
-            CellState::Unchecked => write!(f, "{}", "✅"),
+            CellState::Checked => write!(f, "{}", "✅"),
+            CellState::Unchecked => write!(f, "{}", "⬜"),
         }
     }
 }
